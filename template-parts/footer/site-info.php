cdba88fe506e64142ce4ecc9fbbbe52f7fd27125
Bart Lauwers <blauwers@isc2-austin-chapter.org>
2018-03-24 08:59:11 -0500

    Updated per (ISC)2 Chapter guidance issued 3/12/2018

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -5,13 +5,13 @@
  * @package WordPress
  * @subpackage ISC2_Chapter_Template
  * @since 1.0
- * @version 1.0
+ * @version 1.1
  */
 
 ?>
 <div class="site-info">
 <img class="colored-squares" src="<?php echo get_theme_file_uri('images/footer/colored-squares.png'); ?>" alt="">
 <span class="copyright">
-&copy; Copyright 1996-<?php echo date("Y"); ?>. (ISC)², Inc. All Rights Reserved.<br><br> All contents of this site constitute the property of (ISC)², Inc. and may not be copied, reproduced or distributed without prior written permission. (ISC)², CISSP, SSCP, CCSP, CAP, CSSLP, HCISPP, CCFP, CISSP-ISSAP, CISSP-ISSEP, CISSP-ISSMP and CBK are registered certification marks of (ISC)², Inc.
+&copy; Copyright <?php echo date("Y"); ?>, <?php bloginfo( 'name' ); ?>. All Rights Reserved. (ISC)&sup2;, CISSP, SSCP, CAP, ISSAP, ISSEP, ISSMP, CSSLP, HCISPP and CBK are registered certification, service, and trademarks of International Information System Security Certification Consortium, Inc. <br><br>Disclaimer: (ISC)&sup2; does not own, operate, or moderate this website. All content of this site, exclusive of licensed trademarks and trade dress is the property of <?php bloginfo( 'name' ); ?>, which is not owned, managed, or controlled by (ISC)&sup2; and operates independent of (ISC)&sup2;.”  
 </span>
 </div>
